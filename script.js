5cb9b7fcce13ac171730554f6c6ac4415ab3989c
Искандер <61619826+kan5@users.noreply.github.com>
2023-09-16 21:16:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 ﻿window.addEventListener("load", (event) => {
   new cursoreffects.rainbowCursor();
 });
+function muteMe(elem) {
+//    elem.muted = true;
+    elem.pause();
+}
+
+// Try to mute all video and audio elements on the page
+function mutePage() {
+    document.querySelectorAll("video, audio").forEach((elem) => muteMe(elem));
+}
